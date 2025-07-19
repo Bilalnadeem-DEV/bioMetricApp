ebe142c5d05ffd41a754bac6687f306cb08245b3
Bilal <bilal.nadeem@glowfishlabs.com>
2025-07-20 01:31:54 +0500

    removed model

--- assets/models/index.js
+++
@@ -1,7 +0,0 @@
-// This file helps React Native bundle the ONNX model
-// Reference the model file so it gets included in the bundle
-export const MODEL_PATH = './best.onnx';
-
-// For React Native to recognize and bundle the model
-// We need to reference it explicitly
-require('./best.onnx'); 
